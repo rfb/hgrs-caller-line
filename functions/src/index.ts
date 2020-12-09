1e712d88277f6ead966be104883e56dc278f53be
Ryan Barber <rfb@skyscraper.nu>
2020-12-09 06:04:46 -0800

    logging of params

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,38 @@
 import * as functions from 'firebase-functions';
+import * as nunjucks from 'nunjucks';
+import * as path from 'path';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+nunjucks.configure(path.join(__dirname, '../templates'))
+
+export const incomingCall = functions.https.onRequest((request, response) => {
+  const { params } = request;
+
+  functions.logger.info({
+    CallSid: params.CallSid,
+    AccountSid: params.AccountSid,
+    From: params.From,
+    To: params.To,
+    CallStatus: params.CallStatus,
+    ApiVersion: params.ApiVersion,
+    Direction: params.Direction,
+    ForwardedFrom: params.ForwardedFrom,
+    CallerName: params.CallerName,
+    ParentCallSid: params.ParentCallSid,
+    FromCity: params.FromCity,
+    FromState: params.FromState,
+    FromZip: params.FromZip,
+    FromCountry: params.FromCountry,
+    ToCity: params.ToCity,
+    ToState: params.ToState,
+    ToZip: params.ToZip,
+    ToCountry: params.ToCountry
+  });
+
+  response.contentType('application/xml');
+  response.send(nunjucks.render('incomingCall.njk'));
+});
+
+export const recordingComplete = functions.https.onRequest((_request, response) => {
+  response.contentType('application/xml');
+  response.send(nunjucks.render('recordingComplete.njk'));
+});
